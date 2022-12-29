78e96fb74867e5f463efef27c95fb9140c7fbf0b
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2022-12-29 13:49:57 +0100

    added file dialog and window title

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -17,11 +17,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 	, ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+
+	this->setWindowTitle("CasToSio");
 }
 
 MainWindow::~MainWindow()
@@ -32,5 +36,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionOpen_CAS_Image_A8CAS_triggered()
 {
+	static QString last_dir = "";
+
+	if(last_dir.isEmpty())
+		last_dir = QDir::homePath();
+
+	QString filename = QFileDialog::getOpenFileName(this, tr("Atari CAS File open "), last_dir, tr("CAS Files ") + "(*.cas);;" + tr("All Files ") + "(*.*)", 0, QFileDialog::DontUseNativeDialog);
+	if(filename != "")
+	{
+		QFileInfo dir(filename);
+		last_dir = dir.absoluteDir().absolutePath();
+		qDebug("%s", last_dir.toLatin1().data());
+	}
 }
 
